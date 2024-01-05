a05e2310b898b89f3bf2dbe7dd64a7f740078d9a
d-bytebase <d@bytebase.com>
2024-01-04 18:32:13 -0800

    feat: change the prefix to

--- go/base/context_test.go
+++ go/base/context_test.go
@@ -22,22 +22,22 @@ func TestGetTableNames(t *testing.T) {
 	{
 		context := NewMigrationContext()
 		context.OriginalTableName = "some_table"
-		require.Equal(t, "_some_table_del", context.GetOldTableName())
-		require.Equal(t, "_some_table_gho", context.GetGhostTableName())
-		require.Equal(t, "_some_table_ghc", context.GetChangelogTableName(), "_some_table_ghc")
+		require.Equal(t, "~some_table_del", context.GetOldTableName())
+		require.Equal(t, "~some_table_gho", context.GetGhostTableName())
+		require.Equal(t, "~some_table_ghc", context.GetChangelogTableName(), "~some_table_ghc")
 	}
 	{
 		context := NewMigrationContext()
 		context.OriginalTableName = "a123456789012345678901234567890123456789012345678901234567890"
-		require.Equal(t, "_a1234567890123456789012345678901234567890123456789012345678_del", context.GetOldTableName())
-		require.Equal(t, "_a1234567890123456789012345678901234567890123456789012345678_gho", context.GetGhostTableName())
-		require.Equal(t, "_a1234567890123456789012345678901234567890123456789012345678_ghc", context.GetChangelogTableName())
+		require.Equal(t, "~a1234567890123456789012345678901234567890123456789012345678_del", context.GetOldTableName())
+		require.Equal(t, "~a1234567890123456789012345678901234567890123456789012345678_gho", context.GetGhostTableName())
+		require.Equal(t, "~a1234567890123456789012345678901234567890123456789012345678_ghc", context.GetChangelogTableName())
 	}
 	{
 		context := NewMigrationContext()
 		context.OriginalTableName = "a123456789012345678901234567890123456789012345678901234567890123"
 		oldTableName := context.GetOldTableName()
-		require.Equal(t, "_a1234567890123456789012345678901234567890123456789012345678_del", oldTableName)
+		require.Equal(t, "~a1234567890123456789012345678901234567890123456789012345678_del", oldTableName)
 	}
 	{
 		context := NewMigrationContext()
@@ -46,15 +46,15 @@ func TestGetTableNames(t *testing.T) {
 		longForm := "Jan 2, 2006 at 3:04pm (MST)"
 		context.StartTime, _ = time.Parse(longForm, "Feb 3, 2013 at 7:54pm (PST)")
 		oldTableName := context.GetOldTableName()
-		require.Equal(t, "_a1234567890123456789012345678901234567890123_20130203195400_del", oldTableName)
+		require.Equal(t, "~a1234567890123456789012345678901234567890123_20130203195400_del", oldTableName)
 	}
 	{
 		context := NewMigrationContext()
 		context.OriginalTableName = "foo_bar_baz"
 		context.ForceTmpTableName = "tmp"
-		require.Equal(t, "_tmp_del", context.GetOldTableName())
-		require.Equal(t, "_tmp_gho", context.GetGhostTableName())
-		require.Equal(t, "_tmp_ghc", context.GetChangelogTableName())
+		require.Equal(t, "~tmp_del", context.GetOldTableName())
+		require.Equal(t, "~tmp_gho", context.GetGhostTableName())
+		require.Equal(t, "~tmp_ghc", context.GetChangelogTableName())
 	}
 }
 
